d044a70a47abe83f52b887292d388ef81f55cc1a
Heungsub Lee <heungsub@subl.ee>
2025-10-09 23:07:30 +0900

    Add comments

--- cmd/vs-goverter/convgen.go
+++ cmd/vs-goverter/convgen.go
@@ -10,15 +10,21 @@ import (
 )
 
 var mod = convgen.Module(
+	// Global glossary
 	convgen.RenameReplace("", "", "Id", "ID"),
 	convgen.RenameReplace("", "", "Url", "URL"),
+
+	// Custom conversion by inline functions
 	convgen.ImportFunc(func(u unique) string { return u.String() }),
 	convgen.ImportFunc(func(t time.Time) int64 { return t.Unix() }),
 )
 
 var ConvgenVersion = convgen.StructErr[User, api.User](mod,
+	// Split Name into Firstname and Lastname by custom functions
 	convgen.MatchFuncErr(User{}.Name, api.User{}.Firstname, firstname),
 	convgen.MatchFuncErr(User{}.Name, api.User{}.Lastname, lastname),
+
+	// Match manually
 	convgen.Match(User{}.CreateTime, api.User{}.CreatedAt),
 )
 
